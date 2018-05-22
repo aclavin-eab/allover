0c30dd4a12946d015dca8c6efae7551c67966677
Andrew Clavin <andrewclavin8@gmail.com>
2018-05-22 19:51:13 -0400

    data models populating in db

--- dubuffet/model/index.js
+++ dubuffet/model/index.js
@@ -1,1 +1,23 @@
 console.log('data structures on')
+const Sequelize = require('sequelize')
+
+const db = require('./database')
+
+const Artwork = db.define('artwork', {
+    title: {
+        type: Sequelize.STRING,
+        allowNull: false
+    }
+})
+
+const Artist = db.define('artist', {
+    Name: {
+        type: Sequelize.STRING,
+        allowNull: false
+    }
+})
+
+Artwork.belongsTo(Artist)
+Artist.hasMany(Artwork)
+
+module.exports = db
