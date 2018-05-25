d5aa1d1948108c40a351b9bd593074843d98faaa
Andrew Clavin <andrewclavin8@gmail.com>
2018-05-24 21:57:36 -0400

    models up to story specs

--- dubuffet/models/artist.js
+++ dubuffet/models/artist.js
@@ -5,6 +5,17 @@ const Artist = db.define('artist', {
     name: {
         type: Sequelize.STRING,
         allowNull: false
+    },
+    origin: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    imageUrl: {
+        type: Sequelize.STRING,
+        defaultValue: 'https://www.artline.com/php/images/julian-stanczak-self-portrait-artline.jpg'
+    },
+    bio: {
+        type: Sequelize.TEXT
     }
 })
 
--- dubuffet/models/artwork.js
+++ dubuffet/models/artwork.js
@@ -5,7 +5,31 @@ const Artwork = db.define('artwork', {
     title: {
         type: Sequelize.STRING,
         allowNull: false
+    },
+    medium: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    contact: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate:{
+            isEmail: true
+        }
+    },
+    imageUrl: {
+        type: Sequelize.STRING,
+        defaultValue: 'https://www.moma.org/d/assets/W1siZiIsIjIwMTUvMTAvMjEvNWZ6cDZmMzFsNl9qYWNrc29ucG9sbG9jay5qcGciXSxbInAiLCJjb252ZXJ0IiwiLXJlc2l6ZSAyMDAweDIwMDBcdTAwM2UiXV0/jacksonpollock.jpg?sha=802baec91667ec13'
+    },
+    rating: {
+        type: Sequelize.FLOAT,
+        allowNull: false,
+        validate: {
+            min: 0.0,
+            max: 5.0
+        }
     }
+
 })
 
 module.exports = Artwork;
