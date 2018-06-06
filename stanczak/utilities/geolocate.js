b0110902137221b0facb1ec433e3a3aac631ea67
Andrew Clavin <andrewclavin8@gmail.com>
2018-06-06 00:53:37 -0400

    logs for testing mobile heroku

--- stanczak/utilities/geolocate.js
+++ stanczak/utilities/geolocate.js
@@ -1,10 +1,15 @@
 const geolocate = function(){
+    console.log('running geolocate')
     return new Promise((resolve, reject) => {
+        console.log('in geo promise')
         if("geolocation" in navigator){
+            console.log('in the geolocation existing!')
             navigator.geolocation.getCurrentPosition(position => {
+                console.log("about to resolve w", position)
                 resolve(position)
             });
         } else {
+            console.log('nah, about to reject')
             reject("you don't have geolocation!")
         }
     })
