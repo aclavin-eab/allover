a1ed72f4cae470723fb317cdbc9dcedd07616e7a
Andrew Clavin <andrewclavin8@gmail.com>
2018-09-03 13:37:40 -0400

    feature(user routes): users and login routes added

--- dubuffet/routes/index.js
+++ dubuffet/routes/index.js
@@ -3,6 +3,7 @@ const router = require('express').Router();
 router.use('/artwork', require('./artwork'))
 router.use('/artists', require('./artists'))
 router.use('/locations', require('./locations'))
+router.use('/users', require('./users'))
 
 router.use((req, res, next) => {
     res.status(404).send('Not found');
